ff068684c43967127b584b441fbe1cb17f72afed
cooladj <Anthony.dewayne.jackson@gmail.com>
2023-12-02 13:51:46 -0500

    Added Movement and amations

---
+++ My project (2)/Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator animator;
+    public CharacterController controller;
+    public float speed = 36f;
+    Vector3 velocity;
+    public float booster = 2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        if (Input.GetKey(KeyCode.Space ))
+        {
+            controller.Move(move * (speed* 2) * Time.deltaTime);
+        }
+        else
+        {
+            controller.Move(move * speed * Time.deltaTime);
+        }
+        controller.Move(velocity * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.R) && transform.rotation.z==180)
+        {
+     
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && transform.rotation.z == 0)
+        {
+            
+        }
+    }
+    
+}
